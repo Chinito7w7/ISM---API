7175ab3c0ca774d33c56321c6658ae719efe95a3
Chinito7w7 <142604040+Chinito7w7@users.noreply.github.com>
2026-02-20 15:16:03 -0300

    Se agrego la posibilidad de utilizar querys como limit y page en el endpoint para obtener la lista de todos los productos

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -22,7 +22,16 @@ export const createProduct = async (req, res) => {
 
 export const getProducts = async (req, res) => {
   try {
-    const products = await Product.find({ owner: req.user.id });
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find({ owner: req.user.id })
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
     res.json(products);
   } catch (error) {
     res.status(500).json({ message: "Error al buscar productos" });
