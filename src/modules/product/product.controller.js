649b954dfd9794313261f291474d7e6d5ad7b935
Chinito7w7 <142604040+Chinito7w7@users.noreply.github.com>
2026-02-25 17:14:37 -0300

    Se modifico la respuesta del getProducts para mejor implementacion en el frontend

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -59,24 +59,11 @@ export const createProduct = async (req, res) => {
 // };
 export const getProducts = async (req, res) => {
   try {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-
-    const skip = (page - 1) * limit;
-
-    const [products, total] = await Promise.all([
-      Product.find({ owner: req.user.id })
-        .skip(skip)
-        .limit(limit)
-        .sort({ createdAt: -1 }),
-      Product.countDocuments({ owner: req.user.id }),
-    ]);
-
-    res.json({
-      products,
-      total,
-      page,
+    const products = await Product.find({ owner: req.user.id }).sort({
+      createdAt: -1,
     });
+
+    res.json(products);
   } catch (error) {
     res.status(500).json({ message: "Error al buscar productos" });
   }
