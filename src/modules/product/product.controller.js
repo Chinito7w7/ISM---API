6e2782b2787ff4c71d2cfe4db982416992d433bc
Chinito7w7 <142604040+Chinito7w7@users.noreply.github.com>
2026-02-25 16:21:50 -0300

    Se modifico la respuesta de la accion getProducts porque no acordaba con la respuesta que esperaba el frontend

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -40,6 +40,23 @@ export const createProduct = async (req, res) => {
   }
 };
 
+// export const getProducts = async (req, res) => {
+//   try {
+//     const page = Number(req.query.page) || 1;
+//     const limit = Number(req.query.limit) || 10;
+
+//     const skip = (page - 1) * limit;
+
+//     const products = await Product.find({ owner: req.user.id })
+//       .skip(skip)
+//       .limit(limit)
+//       .sort({ createdAt: -1 });
+
+//     res.json(products);
+//   } catch (error) {
+//     res.status(500).json({ message: "Error al buscar productos" });
+//   }
+// };
 export const getProducts = async (req, res) => {
   try {
     const page = Number(req.query.page) || 1;
@@ -47,12 +64,19 @@ export const getProducts = async (req, res) => {
 
     const skip = (page - 1) * limit;
 
-    const products = await Product.find({ owner: req.user.id })
-      .skip(skip)
-      .limit(limit)
-      .sort({ createdAt: -1 });
+    const [products, total] = await Promise.all([
+      Product.find({ owner: req.user.id })
+        .skip(skip)
+        .limit(limit)
+        .sort({ createdAt: -1 }),
+      Product.countDocuments({ owner: req.user.id }),
+    ]);
 
-    res.json(products);
+    res.json({
+      products,
+      total,
+      page,
+    });
   } catch (error) {
     res.status(500).json({ message: "Error al buscar productos" });
   }
